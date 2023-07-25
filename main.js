43a9c598298786c26dc5b737776ae08b00b471c2
MatrixWizard <88213473+MatrixWizard@users.noreply.github.com>
2023-07-25 17:06:46 +0100

    v0.1

--- main.js
+++ main.js
@@ -1,73 +1,39 @@
-Hooks.on("createChatMessage", (chatMessage) => {
-  console.log(chatMessage.blind)
-  if (!game.settings.get("pf2qr", "ShowPlayersResults")) {
-      if (chatMessage.flavor.includes("Skill Check")){console.log("THIS IS A CHECK ROLL")}{
-          chatMessage.applyRollMode({chatData: chatMessage.blind, rollMode: "BLIND"})
-      }
-      // chatMessage.user = game.users.find(u => u.isGM)._id; //Imitating GM so that we don't see our own message to the GM, in the case it is a player rolling.
-      // chatMessage.speaker = ChatMessage.getSpeaker({ user: game.user });
-  } 
+//CONFIG.debug.hooks = true
+import { registerSettings } from "./scripts/settings.js";
 
-  // let chatData = { content: game.i18n.format(details.chatString, { name: this.actor.name }), speaker };
-// 		ChatMessage.applyRollMode(chatData, roll.options.rollMode);
-// 		await ChatMessage.create(chatData);
+Hooks.on("preCreateChatMessage", async (rawMessage) => {
+  console.log(rawMessage)
+  if (rawMessage.flavor.includes("Skill Check")){
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideAcrobatics") && rawMessage.flavor.includes("Acrobatics")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideArcana") && rawMessage.flavor.includes("Arcana")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideAthletics") && rawMessage.flavor.includes("Athletics")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideCrafting") && rawMessage.flavor.includes("Crafting")){ rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideDeception") && rawMessage.flavor.includes("Deception")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideDiplomacy") && rawMessage.flavor.includes("Diplomacy")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideIntimidation") && rawMessage.flavor.includes("Intimidation")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideLore") && rawMessage.flavor.includes("Lore")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideMedicine") && rawMessage.flavor.includes("Medicine")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideNature") && rawMessage.flavor.includes("Nature")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideOccultism") && rawMessage.flavor.includes("Occultism")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hidePerformance") && rawMessage.flavor.includes("Performance")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideReligion") && rawMessage.flavor.includes("Religion")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideSociety") && rawMessage.flavor.includes("Society")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideStealth") && rawMessage.flavor.includes("Stealth")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideSurvival") && rawMessage.flavor.includes("Survival")){rawMessage.applyRollMode("blindroll");}
+    if(game.settings.get("Tsamys-Secret-Rolls", "hideThievery") && rawMessage.flavor.includes("Thievery")){rawMessage.applyRollMode("blindroll");}
+    console.log("THIS IS A CHECK ROLL")
+  
+  }
 
-  // if (game.user.isGM || game.settings.get("pf2qr", "PlayersCanQuickRoll")) {
-  //     if (game.combat === null || (game.user.data._id === game.users.contents.find(u => u.isGM).data._id) || !game.settings.get("pf2qr", "RollOnTurn") || (game.combat.combatant.players[0]?.data._id === game.user.data._id)) {
-  //         if (!chatMessage.content) return true;
-  //         if (!chatMessage.content.flavor) return true;
-  //         if (chatMessage.content.flavor.includes('Attack Roll') && message.content.user === game.user.data._id) compareAttacks(message);
-  //         if (chatMessage.content.flavor.includes('Strike:') && message.content.user === game.user.data._id) compareAttacks(message);
-  //     }
-  // }
-});
+})
 
 // Handle-socket
 Hooks.on('ready', () => {
   console.log("Registering...")
-  game.socket.on('module.pf2qr', async (data) => {
+  registerSettings();
+  game.socket.on('module.Tsamys-Secret-Rolls', async (data) => {
       if (game.user.isGM && data) showResults(data);
     });
   })
 
   
-Hooks.on('ready', () => {
-  [
-      {
-          name: "ShowPlayersResults",
-          hint: "Whether players should see the results of rolls. Private/Blind rolls will serve a similar function soon(TM).",
-          scope: "world",
-          default: true,
-          type: Boolean
-      }, 
-         {
-          name: "RollOnTurn",
-          hint: "Players can only roll on their turn in combat. Can roll whenever if not in combat.",
-          scope: "world",
-          default: false,
-          type: Boolean
-      },  {
-          name: "RollDamageOnHit",
-          hint: "Automatically rolls damage for any Strike that hits.",
-          scope: "world",
-          default: false,
-          type: Boolean
-      }, {
-          name: "OnlyAutoRollGmDamage",
-          hint: "If enabled, automatic damage rolling from strikes will only happen when it is the GM making the strike.",
-          scope: "world",
-          default: true,
-          type: Boolean
-      }
-  ].forEach((setting) => {
-      let options = {
-          name: setting.name,
-          hint: setting.hint,
-          scope: setting.scope,
-          config: true,
-          default: setting.default,
-          type: setting.type,
-      };
-      game.settings.register("pf2qr", setting.name, options);
-  });
-});
---
+++ scripts/settings.js
@@ -0,0 +1,138 @@
+export const registerSettings = function() {
+  console.log("I AM HERE")
+  game.settings.register("Tsamys-Secret-Rolls", "hideAcrobatics", {
+    name: "Hide Acrobatics",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideArcana", {
+    name: "Hide Arcana",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideAthletics", {
+    name: "Hide Athletics",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideCrafting", {
+    name: "Hide Crafting",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideDeception", {
+    name: "Hide Deception",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideDiplomacy", {
+    name: "Hide Diplomacy",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideIntimidation", {
+    name: "Hide Intimidation",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideLore", {
+    name: "Hide Lore",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideMedicine", {
+    name: "Hide Medicine",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideNature", {
+    name: "Hide Nature",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideOccultism", {
+    name: "Hide Occultism",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hidePerformance", {
+    name: "Hide Performance",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideReligion", {
+    name: "Hide Religion",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideSociety", {
+    name: "Hide Society",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideStealth", {
+    name: "Hide Stealth",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideSurvival", {
+    name: "Hide Survival",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
+  game.settings.register("Tsamys-Secret-Rolls", "hideThievery", {
+    name: "Hide Thievery",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+}
